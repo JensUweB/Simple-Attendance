984e7af0cc4119ae21c65c072e23725933e5756e
JensUweB <58864964+JensUweB@users.noreply.github.com>
2021-11-12 17:01:07 +0100

    chore: upgrade to angular 11

--- src/app/core/attendance/pages/attendance-archive/attendance-archive.component.spec.ts
+++ src/app/core/attendance/pages/attendance-archive/attendance-archive.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
 
 import { AttendanceArchiveComponent } from './attendance-archive.component';
@@ -7,7 +7,7 @@ describe('TrainingArchiveComponent', () => {
   let component: AttendanceArchiveComponent;
   let fixture: ComponentFixture<AttendanceArchiveComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ AttendanceArchiveComponent ],
       imports: [IonicModule.forRoot()]
